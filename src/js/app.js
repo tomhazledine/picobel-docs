8aa82fb7effe19d6dfe15f87fc182efda2ff65c8
Tom Hazledine <tom@tomhazledine.com>
2024-01-26 17:30:43 +0000

    importing basic Picobel content

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,37 @@
-console.log("Hello World!");
+import picobel from "picobel";
+
+const homepageExamples = [
+    {
+        node: document.querySelector("#example-01"),
+        theme: "bare",
+        components: ["playPause", "progress"]
+    },
+    {
+        node: document.querySelector("#example-02"),
+        theme: "default"
+    },
+    {
+        node: document.querySelector("#example-03"),
+        theme: "bare",
+        components: ["playPause", "progress"]
+    },
+    {
+        node: document.querySelector("#example-04"),
+        theme: "crazy",
+        components: [
+            "playPause",
+            "mute",
+            ["title", "artist"],
+            ["progress", ["timer", "duration"]]
+        ]
+    }
+];
+
+homepageExamples.forEach(example => {
+    if (!example.node) return;
+    picobel({
+        theme: example.theme,
+        components: example.components || false,
+        context: example.node
+    });
+});
